96a70afee195caab62a04749c14b7248ef497a96
ahmed3oun <ahmedoun199@gmail.com>
2025-07-19 17:21:38 +0100

    fix issue : expiration ms

--- start-minikube.sh
+++ start-minikube.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 minikube stop
-# Start Minikube with NGINX Ingress enabled
-minikube start --addons=ingress
 
-minikube tunnel
+minikube delete
 
-kubectl create secret generic jwt-key --from-literal=JWT_KEY=${openssl rand --hex 32}
+# Start Minikube with NGINX Ingress and storage-provisioner enabled
+minikube start  --addons=ingress --addons=storage-provisioner
 
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.13.0/deploy/static/provider/cloud/deploy.yaml
+
+kubectl create secret generic jwt-key --from-literal=JWT_KEY=$(openssl rand --hex 32)
+
 
 # Apply the NGINX Ingress Controller manifest application manifests
-kubectl apply -f ./infra/k8s/.
 
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.13.0/deploy/static/provider/cloud/deploy.yaml
+kubectl apply -f ./infra/k8s/
 # Get the status of the Minikube cluster
 minikube status
 

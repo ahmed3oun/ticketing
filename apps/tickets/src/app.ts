3579dad0666912ad49ba3b056de450a7830da8d1
ahmed3oun <ahmedoun199@gmail.com>
2025-11-04 12:09:55 +0100

    add rate limting feat

--- apps/tickets/src/app.ts
+++ apps/tickets/src/app.ts
@@ -1,11 +1,14 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
 import compression from 'compression';
 import cors from 'cors';
 import apiRouter from './routes/api';
 import cookieSession from "cookie-session";
+import Redis, { RedisKey } from "ioredis";
+import configService from "./utils/config/config-service";
 
 
 const app: Express = express();
+const redis = new Redis(configService.getOrElseThrow('REDIS_HOST'))
 app.set('trust proxy', true);
 
 app.use(express.json());
@@ -20,6 +23,21 @@ app.use(compression());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use('public', express.static(__dirname + '/public'));
+
+const RATE_LIMIT = 100; // requests
+const WINDOW = 60; // seconds
+
+app.use(async (req: Request, res: Response, next: NextFunction) => {
+    const key: RedisKey = `rate:${req.ip}`;
+    const count = await redis.incr(key);
+    console.log(`*****${key} => ${count}******`);
+
+    if (count === 1) await redis.expire(key, WINDOW);
+    if (count > RATE_LIMIT)
+        return res.status(429).send({ message: 'Too many requests' }) as any
+
+    next()
+})
 
 // Define a simple health check route
 app.get('/api/v1/tickets/health', (req: Request, res: Response) => {
